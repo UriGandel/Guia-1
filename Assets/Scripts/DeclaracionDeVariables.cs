c78271b96ebcdb59a1f9518cd596b9801762452c
Uriel Gandelman Rothschild <urigandelman@gmail.com>
2025-03-17 10:41:41 -0300

    Ejercicio 1 terminado

--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class DeclaracionDeVariables : MonoBehaviour
 {
+    int edadUsuario;
+    float sueldoUsuario;
+    string domicilio;
+    bool estaAprobado;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Inicializo las variables
+        edadUsuario = 45;
+        sueldoUsuario = 1000.00f;
+        domicilio = "Libertador 6500";
+        estaAprobado = true;
+
+        // Imprimo las variables en consola
+        Debug.Log(edadUsuario);
+        Debug.Log(sueldoUsuario);
+        Debug.Log(domicilio);
+        Debug.Log(estaAprobado);
     }
 
     // Update is called once per frame
