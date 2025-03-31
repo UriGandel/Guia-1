3ef944da64ec2bcc7530ae29a44f2456ff33ea34
Uriel Gandelman Rothschild <73185410+UriGandel@users.noreply.github.com>
2025-03-31 10:39:28 -0300

    Guia 2: Ejercicios 1 y 2

---
+++ Assets/Scripts/SumaYProducto.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SumaYProducto : MonoBehaviour
+{
+    int num1 = 4;
+    int num2 = 5;
+    int resultado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultado = num1 + num2;
+        Debug.Log("El resultado de la suma es: " + resultado);
+        resultado = num1 * num2;
+        Debug.Log("El resultado de la multiplicacion es: " + resultado);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
